f26fc6d6d089bdb42bc7d6dd70df4828d28e7ec9
chaitanyakumar167 <chaithanyakumar167@gmail.com>
2023-07-29 20:54:01 +0530

    my first response

--- first-app.js
+++ first-app.js
@@ -1,7 +1,30 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log("chaithanya kumar");
+  const url = req.url;
+  if (url == "/home") {
+    res.setHeader("Content-Type", "text/html");
+    res.write(
+      "<html><head><title>My first page</title></head><body><h1>Welcome home</h1></body></html>"
+    );
+    res.end();
+  }
+  if (url == "/about") {
+    res.setHeader("Content-Type", "text/html");
+    // we can write like these also (in multiple lines)
+    res.write("<html>");
+    res.write("<head><title>My first page</title></head>");
+    res.write("<body><h1>Welcome to About Us page</h1></body>");
+    res.write("</html>");
+    res.end();
+  }
+  if (url == "/node") {
+    res.setHeader("Content-Type", "text/html");
+    res.write(
+      "<html><head><title>My first page</title></head><body><h1>Welcome to my Node Js project</h1></body></html>"
+    );
+    res.end();
+  }
 });
 
 server.listen(4000);
