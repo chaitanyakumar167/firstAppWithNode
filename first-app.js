eb4836f2395f891427bf18ecf8deb9b44b6774fa
chaitanyakumar167 <chaithanyakumar167@gmail.com>
2023-07-29 14:48:18 +0530

    second commit

--- first-app.js
+++ first-app.js
@@ -1,3 +1,13 @@
-console.log("Hello World");
-// const fs = require("fs");
-// fs.writeFileSync("hello.txt", "Hello World");
+const multiply = (a, b) => {
+  return a * b;
+};
+// console.log(multiply(2, 3));
+
+const student = {
+  name: "chaithanya",
+  age: 26,
+  greet: function () {
+    console.log("Hi, I am " + this.name);
+  },
+};
+student.greet();
