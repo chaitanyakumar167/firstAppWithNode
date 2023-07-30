1f5de9b869d2367a972da097deccfa91c24b1ce4
chaitanyakumar167 <chaithanyakumar167@gmail.com>
2023-07-30 20:43:35 +0530

    Enter Message app reading and writing using node

--- first-app.js
+++ first-app.js
@@ -1,30 +1,36 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
-  if (url == "/home") {
-    res.setHeader("Content-Type", "text/html");
-    res.write(
-      "<html><head><title>My first page</title></head><body><h1>Welcome home</h1></body></html>"
-    );
-    res.end();
-  }
-  if (url == "/about") {
-    res.setHeader("Content-Type", "text/html");
-    // we can write like these also (in multiple lines)
-    res.write("<html>");
-    res.write("<head><title>My first page</title></head>");
-    res.write("<body><h1>Welcome to About Us page</h1></body>");
-    res.write("</html>");
-    res.end();
+  const method = req.method;
+  if (url === "/") {
+    fs.readFile("message.txt", { encoding: "utf-8" }, (err, data) => {
+      res.write(
+        `<html><head><title>Enter Message</title></head><body>${data}<form action="/message" method="POST"><input type="text" name="message" ><button type="submit" >send</button></form></body></html>`
+      );
+      return res.end();
+    });
+  } else if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFile("message.txt", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("location", "/");
+        return res.end();
+      });
+    });
   }
   if (url == "/node") {
-    res.setHeader("Content-Type", "text/html");
     res.write(
       "<html><head><title>My first page</title></head><body><h1>Welcome to my Node Js project</h1></body></html>"
     );
     res.end();
   }
 });
-
 server.listen(4000);
