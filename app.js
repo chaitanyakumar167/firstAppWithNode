454274e21281289e0a5ce8b5d93953489443434a
chaitanyakumar167 <chaithanyakumar167@gmail.com>
2023-08-01 11:50:15 +0530

    added add-product and home route

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require("express");
+const bodyParser = require("body-parser")
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("these is 1st middleware ");
-  next();
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use('/add-product', (req, res, next) => {
+  res.send('<form action="/product" method="POST" ><input type="text" name="productName"><input type="number" name="productSize"><button type="submit">Add Product</button></form>')
 });
-app.use((req, res, next) => {
+
+app.use('/product', (req, res, next)=>{
+  console.log(req.body)
+  res.redirect('/')
+})
+app.use("/" ,(req, res, next) => {
   res.send("<h1>Hello from express</h1>");
 });
 app.listen(4000);
